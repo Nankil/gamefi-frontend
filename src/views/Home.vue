68715bb803253708aed6fe599e4f4e103f304bfa
duantao <2586784220@qq.com>
2022-05-11 22:01:57 +0800

    首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,248 @@
 <template>
-    <div class="dark:text-white">Home</div>
+  <div class="dark:text-white bg-black text-purple-600">
+    <div class="relative h-10">
+      <span class="absolute bottom-0 text-purple-500">最新消息</span>
+    </div>
+    <div class="h-52 flex">
+      <div class="w-3/4 h-full border-2 border-solid border-white">
+        <img src="" alt="" />
+      </div>
+      <div class="w-1/4 flex flex-col">
+        <div class="border-2 border-solid border-white flex-1 text-xs h-1/5">
+          <div class="inline-block">消息</div>
+          <div>时间</div>
+        </div>
+        <div class="border-2 border-solid border-white flex-1 text-xs h-1/5">
+          <div class="inline-block">消息</div>
+          <div>时间</div>
+        </div>
+        <div class="border-2 border-solid border-white flex-1 text-xs h-1/5">
+          <div class="inline-block">消息</div>
+          <div>时间</div>
+        </div>
+        <div class="border-2 border-solid border-white flex-1 text-xs h-1/5">
+          <div class="inline-block">消息</div>
+          <div>时间</div>
+        </div>
+        <div class="border-2 border-solid border-white flex-1 text-xs h-1/5">
+          <div class="inline-block">消息</div>
+          <div>时间</div>
+        </div>
+      </div>
+    </div>
+    <div class="text-right">MORE>></div>
+    <div class="flex flex-row">
+      <div class="flex-1 mr-10">
+        <div class="relative h-10">
+          <span class="absolute bottom-0 text-purple-500">社区金库</span>
+        </div>
+        <div class="border-2 border-solid border-white h-96">
+          <div class="text-center">总资产</div>
+          <div class="text-center border-b-2 border-solid border-white">
+            ≈ $ 10,000.008
+          </div>
+          <div class="mx-3 h-60">
+            <div class="h-1/5 flex">
+              <div class="w-1/6 mx-1 h-full"><img src="" alt="" /></div>
+              <div class="w-1/2">
+                <div class="text-yellow-600">BHD</div>
+                <div class="truncate text-xs">0x7fe….g8ff [ 複製 ]</div>
+              </div>
+              <div class="text-right text-green-500">
+                <div>100,000</div>
+                <div>≈ $ 1,000</div>
+              </div>
+            </div>
+            <div class="h-1/5 flex">
+              <div class="w-1/6 mx-1 h-full"><img src="" alt="" /></div>
+              <div class="w-1/2">
+                <div class="text-yellow-600">BHD</div>
+                <div class="truncate text-xs">0x7fe….g8ff [ 複製 ]</div>
+              </div>
+              <div class="text-right text-green-500">
+                <div>100,000</div>
+                <div>≈ $ 1,000</div>
+              </div>
+            </div>
+            <div class="h-1/5 flex">
+              <div class="w-1/6 mx-1 h-full"><img src="" alt="" /></div>
+              <div class="w-1/2">
+                <div class="text-yellow-600">BHD</div>
+                <div class="truncate text-xs">0x7fe….g8ff [ 複製 ]</div>
+              </div>
+              <div class="text-right text-green-500">
+                <div>100,000</div>
+                <div>≈ $ 1,000</div>
+              </div>
+            </div>
+            <div class="h-1/5 flex">
+              <div class="w-1/6 mx-1 h-full"><img src="" alt="" /></div>
+              <div class="w-1/2">
+                <div class="text-yellow-600">BHD</div>
+                <div class="truncate text-xs">0x7fe….g8ff [ 複製 ]</div>
+              </div>
+              <div class="text-right text-green-500">
+                <div>100,000</div>
+                <div>≈ $ 1,000</div>
+              </div>
+            </div>
+            <div class="h-1/5 flex">
+              <div class="w-1/6 mx-1 h-full"><img src="" alt="" /></div>
+              <div class="w-1/2">
+                <div class="text-yellow-600">BHD</div>
+                <div class="truncate text-xs">0x7fe….g8ff [ 複製 ]</div>
+              </div>
+              <div class="text-right text-green-500">
+                <div>100,000</div>
+                <div>≈ $ 1,000</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex-1">
+        <div class="relative h-10">
+          <span class="absolute bottom-0 text-purple-500">用户数量设计</span>
+        </div>
+        <div
+          class="
+            border-2 border-solid border-white
+            flex flex-col
+            justify-around
+            h-96
+          "
+        >
+          <div class="mx-3 my-1">
+            <div class="text-purple-500">会员注册人数</div>
+            <div class="text-yellow-700 text-xs">10000</div>
+          </div>
+          <div class="mx-3 my-1">
+            <div class="text-purple-500">会员注册人数</div>
+            <div class="text-yellow-700 text-xs">10000</div>
+          </div>
+          <div class="mx-3 my-1">
+            <div class="text-purple-500">会员注册人数</div>
+            <div class="text-yellow-700 text-xs">10000</div>
+          </div>
+          <div class="mx-3 my-1">
+            <div class="text-purple-500">会员注册人数</div>
+            <div class="text-yellow-700 text-xs">10000</div>
+          </div>
+          <div class="mx-3 my-1">
+            <div class="text-purple-500">会员注册人数</div>
+            <div class="text-yellow-700 text-xs">10000</div>
+          </div>
+          <div class="mx-3 my-1">
+            <div class="text-purple-500">会员注册人数</div>
+            <div class="text-yellow-700 text-xs">10000</div>
+          </div>
+          <div class="mx-3 my-1">
+            <div class="text-purple-500">会员注册人数</div>
+            <div class="text-yellow-700 text-xs">10000</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="relative h-14">
+      <span class="absolute bottom-0 text-purple-500">代币经济</span>
+    </div>
+    <div
+      class="flex flex-row border-2 border-solid border-white text-purple-500"
+    >
+      <div class="flex-1 mx-3 my-2">
+        <div class="text-purple-500">母币</div>
+        <div class="border-2 border-solid border-white h-96">
+          <div class="flex border-b-2 border-solid border-white">
+            <div class="w-1/4"><img src="" alt="" /></div>
+            <div>
+              <div>haha</div>
+              <div>haha</div>
+            </div>
+          </div>
+          <div class="mx-8">
+            <div>
+              <div class="flex my-2">
+                <div class="w-1/2">1</div>
+                <div class="text-yellow-600 w-1/2 text-right">1</div>
+              </div>
+              <div class="flex my-2">
+                <div class="w-1/2">1</div>
+                <div class="text-yellow-600 w-1/2 text-right">1</div>
+              </div>
+              <div class="flex my-2">
+                <div class="w-1/2">1</div>
+                <div class="text-yellow-600 w-1/2 text-right">1</div>
+              </div>
+              <div class="flex my-2">
+                <div class="w-1/2">1</div>
+                <div class="text-yellow-600 w-1/2 text-right">1</div>
+              </div>
+              <div class="flex my-2">
+                <div class="w-1/2">1</div>
+                <div class="text-yellow-600 w-1/2 text-right">1</div>
+              </div>
+            </div>
+            <div class="my-5">
+              <div>haha</div>
+              <div>haha</div>
+            </div>
+            <div class="text-yellow-500 my-3">0x5068…Cc2f2b3 [ 複製 ]</div>
+            <div class="flex justify-around">
+              <button>合約機制查看</button>
+              <button>222222</button>
+              <button>3333</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex-1 mx-3 my-2">
+        <div class="text-purple-500">母币</div>
+        <div class="border-2 border-solid border-white h-96">
+          <div class="flex border-b-2 border-solid border-white">
+            <div class="w-1/4"><img src="" alt="" /></div>
+            <div>
+              <div>haha</div>
+              <div>haha</div>
+            </div>
+          </div>
+          <div class="mx-8">
+            <div>
+              <div class="flex my-2">
+                <div class="w-1/2">1</div>
+                <div class="text-yellow-600 w-1/2 text-right">1</div>
+              </div>
+              <div class="flex my-2">
+                <div class="w-1/2">1</div>
+                <div class="text-yellow-600 w-1/2 text-right">1</div>
+              </div>
+              <div class="flex my-2">
+                <div class="w-1/2">1</div>
+                <div class="text-yellow-600 w-1/2 text-right">1</div>
+              </div>
+              <div class="flex my-2">
+                <div class="w-1/2">1</div>
+                <div class="text-yellow-600 w-1/2 text-right">1</div>
+              </div>
+              <div class="flex my-2">
+                <div class="w-1/2">1</div>
+                <div class="text-yellow-600 w-1/2 text-right">1</div>
+              </div>
+            </div>
+            <div class="my-5">
+              <div>haha</div>
+              <div>haha</div>
+            </div>
+            <div class="text-yellow-500 my-3">0x5068…Cc2f2b3 [ 複製 ]</div>
+            <div class="flex justify-around">
+              <button>合約機制查看</button>
+              <button>222222</button>
+              <button>3333</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="h-20 my-10" >项目发展路线图</div>
+    <div class="h-20">合作方</div>
+  </div>
 </template>
