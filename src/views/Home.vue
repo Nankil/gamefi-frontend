376dc085cd61034d5aa038f628b58a1f80d78e93
duantao <2586784220@qq.com>
2022-05-15 16:12:49 +0800

    home struct

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,157 +1,95 @@
-
+<style>
+.center {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+}
+.right {
+  position: absolute;
+  left: 0;
+}
+.nav-container {
+  background-image: url("/imgs/comm_vouch.png");
+  background-size: 100% 100%;
+}
+.container2 {
+  background-image: url("/imgs/user_stat.png");
+  background-size: 100% 100%;
+}
+.container3 {
+  background-image: url("imgs/token2.png");
+  background-size: 100% 100%;
+}
+.container4 {
+  background-image: url("imgs/token1.png");
+  background-size: 100% 100%;
+}
+</style>
 <template>
-  <div class="dark:text-white text-purple-600 h-full bg-black">
-    <div class="relative h-10">
-      <span class="absolute bottom-0 text-purple-500">{{ $t('Home.message') }}</span>
-    </div>
+  <div class="dark:text-white text-purple-600 h-full bg-green-500 px-10">
+    <img src="imgs\new_message.png" alt="" />
     <div class="h-52 flex">
       <div class="w-3/4 h-full border-2 border-solid border-white">
         <img src="" alt="" />
       </div>
       <div class="w-1/4 flex flex-col">
-        <div class="border-2 border-solid border-white flex-1 text-xs h-1/5">
-          <div class="inline-block">消息</div>
-          <div>时间</div>
-        </div>
-        <div class="border-2 border-solid border-white flex-1 text-xs h-1/5">
-          <div class="inline-block">消息</div>
-          <div>时间</div>
-        </div>
-        <div class="border-2 border-solid border-white flex-1 text-xs h-1/5">
-          <div class="inline-block">消息</div>
-          <div>时间</div>
-        </div>
-        <div class="border-2 border-solid border-white flex-1 text-xs h-1/5">
-          <div class="inline-block">消息</div>
-          <div>时间</div>
-        </div>
-        <div class="border-2 border-solid border-white flex-1 text-xs h-1/5">
+        <div
+          v-for="x in 5"
+          :key="x"
+          class="border-2 border-solid border-white flex-1 text-xs h-1/5"
+        >
           <div class="inline-block">消息</div>
           <div>时间</div>
         </div>
       </div>
     </div>
-    <div class="text-right">MORE>></div>
+    <div class="text-right">
+      <img class="inline h-9" src="imgs\more.png" alt="" />
+    </div>
     <div class="flex flex-row">
-      <div class="flex-1 mr-10">
-        <div class="relative h-10">
-          <span class="absolute bottom-0 text-purple-500">{{$t('Home.communitybank')}}</span>
-        </div>
-        <div class="border-2 border-solid border-white h-96">
+      <div class="flex-1 nav-container p-14">
+        <div class="border-2 border-solid border-white w-4/5">
           <div class="text-center">总资产</div>
           <div class="text-center border-b-2 border-solid border-white">
             ≈ $ 10,000.008
           </div>
-          <div class="mx-3 h-60">
-            <div class="h-1/5 flex">
-              <div class="w-1/6 mx-1 h-full"><img src="" alt="" /></div>
-              <div class="w-1/2">
-                <div class="text-yellow-600">BHD</div>
-                <div class="truncate text-xs">0x7fe….g8ff [ 複製 ]</div>
-              </div>
-              <div class="text-right text-green-500">
-                <div>100,000</div>
-                <div>≈ $ 1,000</div>
-              </div>
+
+          <div class="h-1/5 flex" v-for="x in 5" :key="x">
+            <div class="w-1/6 mx-1 h-full"><img src="" alt="" /></div>
+            <div class="w-1/2">
+              <div class="text-yellow-600">BHD</div>
+              <div class="truncate text-xs">0x7fe….g8ff [ 複製 ]</div>
             </div>
-            <div class="h-1/5 flex">
-              <div class="w-1/6 mx-1 h-full"><img src="" alt="" /></div>
-              <div class="w-1/2">
-                <div class="text-yellow-600">BHD</div>
-                <div class="truncate text-xs">0x7fe….g8ff [ 複製 ]</div>
-              </div>
-              <div class="text-right text-green-500">
-                <div>100,000</div>
-                <div>≈ $ 1,000</div>
-              </div>
-            </div>
-            <div class="h-1/5 flex">
-              <div class="w-1/6 mx-1 h-full"><img src="" alt="" /></div>
-              <div class="w-1/2">
-                <div class="text-yellow-600">BHD</div>
-                <div class="truncate text-xs">0x7fe….g8ff [ 複製 ]</div>
-              </div>
-              <div class="text-right text-green-500">
-                <div>100,000</div>
-                <div>≈ $ 1,000</div>
-              </div>
-            </div>
-            <div class="h-1/5 flex">
-              <div class="w-1/6 mx-1 h-full"><img src="" alt="" /></div>
-              <div class="w-1/2">
-                <div class="text-yellow-600">BHD</div>
-                <div class="truncate text-xs">0x7fe….g8ff [ 複製 ]</div>
-              </div>
-              <div class="text-right text-green-500">
-                <div>100,000</div>
-                <div>≈ $ 1,000</div>
-              </div>
-            </div>
-            <div class="h-1/5 flex">
-              <div class="w-1/6 mx-1 h-full"><img src="" alt="" /></div>
-              <div class="w-1/2">
-                <div class="text-yellow-600">BHD</div>
-                <div class="truncate text-xs">0x7fe….g8ff [ 複製 ]</div>
-              </div>
-              <div class="text-right text-green-500">
-                <div>100,000</div>
-                <div>≈ $ 1,000</div>
-              </div>
+            <div class="text-right text-green-500">
+              <div>100,000</div>
+              <div>≈ $ 1,000</div>
             </div>
           </div>
         </div>
       </div>
-      <div class="flex-1">
-        <div class="relative h-10">
-          <span class="absolute bottom-0 text-purple-500">{{$t('Home.usercount')}}</span>
-        </div>
+
+      <div class="flex-1 container2 p-14">
         <div
           class="
             border-2 border-solid border-white
             flex flex-col
             justify-around
-            h-96
           "
         >
-          <div class="mx-3 my-1">
-            <div class="text-purple-500">会员注册人数</div>
-            <div class="text-yellow-700 text-xs">10000</div>
-          </div>
-          <div class="mx-3 my-1">
-            <div class="text-purple-500">会员注册人数</div>
-            <div class="text-yellow-700 text-xs">10000</div>
-          </div>
-          <div class="mx-3 my-1">
-            <div class="text-purple-500">会员注册人数</div>
-            <div class="text-yellow-700 text-xs">10000</div>
-          </div>
-          <div class="mx-3 my-1">
-            <div class="text-purple-500">会员注册人数</div>
-            <div class="text-yellow-700 text-xs">10000</div>
-          </div>
-          <div class="mx-3 my-1">
-            <div class="text-purple-500">会员注册人数</div>
-            <div class="text-yellow-700 text-xs">10000</div>
-          </div>
-          <div class="mx-3 my-1">
-            <div class="text-purple-500">会员注册人数</div>
-            <div class="text-yellow-700 text-xs">10000</div>
-          </div>
-          <div class="mx-3 my-1">
+          <div class="mx-3 my-1" v-for="x in 7" :key="x">
             <div class="text-purple-500">会员注册人数</div>
             <div class="text-yellow-700 text-xs">10000</div>
           </div>
         </div>
       </div>
     </div>
-    <div class="relative h-14">
-      <span class="absolute bottom-0 text-purple-500">{{$t('Home.tokenconomy')}}</span>
-    </div>
+
+    <img src="imgs\economic_bar.png" alt="" />
+
     <div
       class="flex flex-row border-2 border-solid border-white text-purple-500"
     >
-      <div class="flex-1 mx-3 my-2">
-        <div class="text-purple-500">母币</div>
+      <div class="flex-1 container3 p-14">
         <div class="border-2 border-solid border-white h-96">
           <div class="flex border-b-2 border-solid border-white">
             <div class="w-1/4"><img src="" alt="" /></div>
@@ -196,8 +134,7 @@
           </div>
         </div>
       </div>
-      <div class="flex-1 mx-3 my-2">
-        <div class="text-purple-500">母币</div>
+      <div class="flex-1 container4 p-14">
         <div class="border-2 border-solid border-white h-96">
           <div class="flex border-b-2 border-solid border-white">
             <div class="w-1/4"><img src="" alt="" /></div>
@@ -208,23 +145,7 @@
           </div>
           <div class="mx-8">
             <div>
-              <div class="flex my-2">
-                <div class="w-1/2">1</div>
-                <div class="text-yellow-600 w-1/2 text-right">1</div>
-              </div>
-              <div class="flex my-2">
-                <div class="w-1/2">1</div>
-                <div class="text-yellow-600 w-1/2 text-right">1</div>
-              </div>
-              <div class="flex my-2">
-                <div class="w-1/2">1</div>
-                <div class="text-yellow-600 w-1/2 text-right">1</div>
-              </div>
-              <div class="flex my-2">
-                <div class="w-1/2">1</div>
-                <div class="text-yellow-600 w-1/2 text-right">1</div>
-              </div>
-              <div class="flex my-2">
+              <div class="flex my-2" v-for="x in 5" :key="x">
                 <div class="w-1/2">1</div>
                 <div class="text-yellow-600 w-1/2 text-right">1</div>
               </div>
@@ -243,8 +164,8 @@
         </div>
       </div>
     </div>
-    <div class="h-20 my-10 text-4xl text-purple-500">{{$t('Home.PDR')}}</div>
-    <div class="h-20 text-4xl text-purple-500">{{$t('Home.partners')}}</div>
+    <div class="h-20 my-10 text-4xl text-purple-500">{{ $t("Home.PDR") }}</div>
+    <div class="h-20 text-4xl text-purple-500">{{ $t("Home.partners") }}</div>
   </div>
 </template>
 <script>
