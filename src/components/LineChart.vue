c6d30851847c596782b3b796efcf908bc00bb132
turing0 <turing175@gmail.com>
2022-05-17 17:42:05 +0800

    charts

---
+++ src/components/LineChart.vue
@@ -0,0 +1,49 @@
+<template>
+  <Chart
+    :size="{ width: 150, height: 100 }"
+    :data="data"
+    :margin="margin"
+    :direction="direction">
+
+    <template #layers>
+      <Grid strokeDasharray="2,2" />
+      <Line :dataKeys="['name', 'pl']" />
+    </template>
+
+  </Chart>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from 'vue';
+import {Chart, Grid, Line} from 'vue3-charts';
+
+const plByMonth = [
+  {name: 'Jan', pl: 500, avg: 500, inc: 300},
+  {name: 'Feb', pl: 900, avg: 900, inc: 400},
+  {name: 'Apr', pl: 400, avg: 400, inc: 500},
+  {name: 'Mar', pl: 3100, avg: 1300, inc: 700},
+];
+
+export default defineComponent({
+  name: 'LineChart',
+  components: {Chart, Grid, Line},
+  setup() {
+    const data = ref(plByMonth);
+    const direction = ref('horizontal');
+    const margin = ref({
+      left: 0,
+      top: 20,
+      right: 20,
+      bottom: 0,
+    });
+
+    return {data, direction, margin};
+  },
+});
+</script>
+
+<style>
+#app {
+  color: #2ecc71
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,30 +17,33 @@
     <img src="/imgs/spliter1.png" alt="splitter">
 
 
-
     <img src="/imgs/spliter2.png" alt="splitter">
     <img src="/imgs/economic_bar.png " class="fengeimg h-20" alt="" />
 
     <Hometoken />
-    
+    <div class="w-full">
+      <LineChart />
+    </div>
+
     <div class="h-20 my-10 text-4xl text-purple-500">{{ $t("Home.PDR") }}</div>
     <div class="h-20 text-4xl text-purple-500">{{ $t("Home.partners") }}</div>
   </div>
 </template>
 <script>
-import HomeNewMessage from "./HomeNewMessage.vue";
-import Homeswiper from "./Homeswiper.vue";
-import Hometoken from "./Hometoken.vue";
+import HomeNewMessage from './HomeNewMessage.vue';
+import Homeswiper from './Homeswiper.vue';
+import Hometoken from './Hometoken.vue';
+import LineChart from '@/components/LineChart.vue';
 export default {
-  name: "app",
+  name: 'app',
   data() {
-    return { locale: "en" };
+    return {locale: 'en'};
   },
   watch: {
     locale(val) {
       this.$i18n.locale = val;
     },
   },
-  components: { Homeswiper, HomeNewMessage, Hometoken },
+  components: {Homeswiper, HomeNewMessage, Hometoken, LineChart},
 };
 </script>
